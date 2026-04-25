87096089945e3921c61582d2d9af7e8cbdafcdc0
aslesh <asleshashu90@gmail.com>
2026-04-25 21:59:55 +0530

    Fix CORS policy to allow Render frontend

--- backend/index.js
+++ backend/index.js
@@ -16,15 +16,10 @@ if (!fs.existsSync(CSV_FILE_PATH)) {
 }
 
 // Middleware
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://127.0.0.1:5173'];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: '*', // This allows requests from ANY URL (including your new Render frontend)
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
 }));
 
 app.use(express.json());
